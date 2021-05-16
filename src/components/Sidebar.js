6170b8e6c54afd0b413a63ed85b64effd7dddd60
vivekanand27 <vivekanand21@ymail.com>
2021-05-16 20:26:22 +0530

    sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col } from 'react-bootstrap'
+
+import "../App.css";
+
+function Sidebar() {
+   
+    useEffect(() => {
+       
+    }, [])
+
+    
+        return (
+           <Row>
+               <Col >
+             Hi
+               </Col>
+           </Row>
+                
+         
+        )
+  
+
+}
+
+export default Sidebar;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,14 +3,14 @@ import { Row, Col } from 'react-bootstrap'
 
 import "../App.css";
 import News from '../components/News'
+import Sidebar from '../components/Sidebar.js'
 
 function HomeScreen() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        debugger
+    useEffect(() => {        
         fetch("https://api.first.org/data/v1/news")
             .then(res => res.json())
             .then(
@@ -32,6 +32,9 @@ function HomeScreen() {
     } else {
         return (
            <Row>
+               <Col xs={3}>
+                    <Sidebar />
+               </Col>
                <Col  xs={4} sm={4} md={4} lg={4} xl={3}>
                {items.map(item => (
                     <li key={item.id}>
