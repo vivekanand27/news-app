9cbd823576d7ba48a978b8c1d175805a5143e5ac
vivekanand27 <vivekanand21@ymail.com>
2021-05-17 11:03:50 +0530

    changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,34 +1,84 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col } from 'react-bootstrap'
+import React, { useEffect } from "react";
+import { Row, Col, Card, Image} from 'react-bootstrap'
+import {Link} from 'react-router-dom';
 
 import "../App.css";
 
 function Sidebar() {
 
-  let showFeedback = false;
+    let showFeedback = false;
    
     useEffect(() => {
-       
+
     }, [])
 
-  const showFeedbackFunction = () => {
-    showFeedback = !showFeedback;
-  }
+    const showFeedbackFunction = () => {
+        showFeedback = !showFeedback;
+    }
 
-    
-        return (
-           <Row>
-               <Col >
-             Hi
-               </Col>
-               <Col xs={12}>
-               <button onClick={showFeedbackFunction()}>We are listening</button>
-               </Col>
-           </Row>
-                
-         
-        )
-  
+
+    return (
+        <Row>
+            <Col xs={12}>
+                <Card>
+                    <Card.Body>
+                        <Row>
+                            <Col xs={4}><Image src="/modi.jpg" alt="image" fluid roundedCircle /></Col>
+                            <Col xs={8}> <Card.Text as='div'>
+                                <h6>Hi Reader,</h6>
+                            </Card.Text>
+                                <Card.Text>
+                                    <div>
+                                        <div>Here's your news</div>
+                                    </div>
+                                </Card.Text></Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+            <Col xs={12} className="py-4">
+                <Card>
+                    <Card.Body>
+                        <Row>
+                            <Col xs={12}> <Card.Text as='div'>
+                                <center><h5>View Toggle</h5></center>
+                            </Card.Text>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+
+            <Col xs={12} className="py-2">
+                <Card>
+                    <Card.Body>
+                        <Row>
+                            <Col xs={12}>
+                                <Card.Text as='div'>
+                                    <center><h5>Have a Feedback?</h5></center>
+                                </Card.Text>
+                            </Col>
+                            <Col className="py-3">
+                            <center>
+                            <Link to="/feedback" className="btn btn-success">We are listening</Link>
+                                {/* <Button variant="success" onClick={showFeedbackFunction()}>We are listening</Button> */}
+                            </center>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+            <Col xs={12}>
+
+            </Col>
+        </Row>
+
+
+    )
+
 
 }
 
--- src/screens/FeedbackScreen.js
+++ src/screens/FeedbackScreen.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+
 
 function FeedbackScreen() {
 
@@ -9,35 +11,39 @@ function FeedbackScreen() {
 
     return (
         <>
-        <h5>Thank you so much for taking the time!</h5>
-        <small>Please provide the below details!</small>
-        <br/>
-        <br/>
+            <Row>
+                <Col xs={7}> <h5>Thank you so much for taking the time!</h5>
+                    <small>Please provide the below details!</small>
+                </Col>
+                <Col xs={5}>
+                    <Link to="/" className="btn btn-success">Go Back</Link>
+                </Col>
+            </Row>
             <Form>
 
                 <Form.Group controlId="fname">
                     <Form.Label>First Name:</Form.Label>
                     <Row>
                         <Col xs={7} >
-                        <Form.Control size="sm" placeholder="John" />
+                            <Form.Control size="sm" placeholder="John" />
                         </Col>
                     </Row>
-                  
+
                 </Form.Group>
 
                 <Form.Group controlId="lname">
                     <Form.Label>Last Name:</Form.Label>
                     <Row>
                         <Col xs={7} >
-                        <Form.Control size="sm" placeholder="Doe" />
+                            <Form.Control size="sm" placeholder="Doe" />
                         </Col>
                     </Row>
-                   
+
                 </Form.Group>
 
                 <div className="form-group">
                     <label htmlFor="address">Address:</label>
-                    <textarea className="form-control" id="description" rows="4"
+                    <textarea className="form-control" id="description" rows="2"
                         placeholder='Enter your full Postal Address'></textarea>
                 </div>
 
@@ -45,20 +51,20 @@ function FeedbackScreen() {
                     <Form.Label>Country:</Form.Label>
                     <Row>
                         <Col xs={8} >
-                        <Form.Control size="sm" placeholder="India" />
+                            <Form.Control size="sm" placeholder="India" />
                         </Col>
                     </Row>
-                    
+
                 </Form.Group>
 
                 <Form.Group controlId="emailId">
                     <Form.Label>Email ID:</Form.Label>
                     <Row>
                         <Col xs={7} >
-                        <Form.Control size="sm" required type="email" placeholder="example@sample.com" />
+                            <Form.Control size="sm" required type="email" placeholder="example@sample.com" />
                         </Col>
                     </Row>
-                    
+
                 </Form.Group>
 
                 <Form.Group controlId="formBasicCheckbox">
